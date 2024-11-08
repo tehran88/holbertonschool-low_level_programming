c8c3702093575bdece3578291cd272a6fbb94243
tehran88 <tehranabilovaa@gmail.com>
2024-11-08 15:51:03 +0000

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -7,9 +7,9 @@ void print_numbers(void)
 {
     char i;
 
-    for (i = '0'; i <= '9'; i++)   // '0' simvolundan '9' simvoluna qədər dövr
+    for (i = '0'; i <= '9'; i++)   /* '0' simvolundan '9' simvoluna qədər dövr */
     {
-        _putchar(i);   // Çap edirik: 0, 1, 2, ..., 9
+        _putchar(i);  // Çap edirik: 0, 1, 2, ..., 9
     }
     _putchar('\n');  // Yeni xətt simvolu
 }
