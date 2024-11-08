3bf7837bff0f0966cb05fc4199965d5ef7206c80
tehran88 <tehranabilovaa@gmail.com>
2024-11-08 15:32:18 +0000

    3-print.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9 followed by a new line
+ */
+void print_numbers(void)
+{
+    char i;
+
+    for (i = '0'; i <= '9'; i++)   // '0' simvolundan '9' simvoluna qədər dövr
+    {
+        _putchar(i);   // Çap edirik: 0, 1, 2, ..., 9
+    }
+    _putchar('\n');  // Yeni xətt simvolu
+}
+
