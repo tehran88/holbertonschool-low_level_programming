158b1c9d55b02a6d11330fecfd753ad492063aff
tehran88 <tehranabilovaa@gmail.com>
2024-11-08 15:53:40 +0000

    main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_numbers(void);  // print_numbers funksiyasının prototipi
-int _putchar(char c);       // _putchar funksiyasının prototipi
+/* print_numbers funksiyasının prototipi */
+void print_numbers(void);  
+
+/* _putchar funksiyasının prototipi */
+int _putchar(char c);       
 
 #endif /* MAIN_H */
 
