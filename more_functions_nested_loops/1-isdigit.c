dcc184d98483096068c733e16f97f9e65d981b9f
tehran88 <tehranabilovaa@gmail.com>
2024-11-08 13:57:00 +0000

    1-isdigit.c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,19 @@
-int _isupper(int c)
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is a digit (0 through 9)
+ * @c: The character to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
 {
-if (c >= 'A' && c <= 'Z')
-        {
+    if (c >= '0' && c <= '9')
+    {
         return (1);
-        }
-return (0);
+    }
+    else
+    {
+        return (0);
+    }
 }
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,1 +1,6 @@
-int _isupper(int c);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);  // _isupper funksiyasının prototipi
+
+#endif /* MAIN_H */
