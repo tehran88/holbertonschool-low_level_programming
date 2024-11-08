64155cc341c5fe8ecf130aa9ba535fd43d531a6b
tehran88 <tehranabilovaa@gmail.com>
2024-11-08 15:39:12 +0000

    main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int mul(int a, int b); /*`mul` funksiyasının prototipi*/
+void print_numbers(void);  // print_numbers funksiyasının prototipi
+int _putchar(char c);       // _putchar funksiyasının prototipi
 
 #endif /* MAIN_H */
 
