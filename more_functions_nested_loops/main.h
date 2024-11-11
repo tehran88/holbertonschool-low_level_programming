d7697e9cd916f25054bd4179b0cfa7b1a98da9a1
tehran88 <tehranabilovaa@gmail.com>
2024-11-11 12:29:32 +0000

    5

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,6 +3,10 @@
 
 #include<stdio.h>
 void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 int _putchar(char c);
 #endif
-
