09c38b2b22b58ad37b315b9700008bca323c2cff
tehran88 <tehranabilovaa@gmail.com>
2024-11-11 14:36:49 +0000

    test

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,13 @@ void print_rev(char *s)
 {
 int count = 0;
 int i;
-while (s[count]!='\0')
+while (s[count] != '\0')
 {
 	count++;
 }
 for (i = count - 1; i >= 0; i--)
 {
-	write(1,&s[i],1);
+	write(1 ,&s[i], 1);
 }
-write(1,"\n",1);
+write(1, "\n", 1);
 }
