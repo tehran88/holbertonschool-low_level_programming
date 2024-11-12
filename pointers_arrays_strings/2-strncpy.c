ebc961c14bc738f0a6dcff72f9dc5410d1707160
tehran88 <tehranabilovaa@gmail.com>
2024-11-12 12:28:37 +0000

    ggls

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,24 +9,19 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int srclen = 0, i = 0;
-	char *temp = dest, *start = src;
-
-	while (*src)
+	int i = 0;
+    
+	while (i < n && src[i] != '\0')
 	{
-		srclen++;
-		src++;
+	dest[i] = src[i];
+	i++;
 	}
 
-	srclen++;
+	while (i < n)
+	{
+	dest[i] = '\0';
+	i++;
+	}
 
-	if (n > srclen)
-		n = srclen;
-
-	src = start;
-
-	for (; i < n; i++)
-		*dest++ = *src++;
-
-	return (temp);
+	return dest;
 }
