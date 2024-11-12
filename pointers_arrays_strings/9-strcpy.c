ce97a525d7d4c81723efe8aab920259d66e47bf8
tehran88 <tehranabilovaa@gmail.com>
2024-11-12 10:49:51 +0000

    gg

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -11,9 +11,7 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	char *aux = dest;
-
-	while (*src)
-		*dest++ = *src++;
+	char *aux = dest;	while (*src)
+		*dest++ = *src++;	*dest = '\0';
 	return (aux);
 }
