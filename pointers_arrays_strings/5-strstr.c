aba6e1b03c1af45ea9595f5981b6ec1747e44a0c
tehran88 <tehranabilovaa@gmail.com>
2024-11-14 13:40:22 +0000

    memset

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -7,7 +7,7 @@
  * Return: A pointer to the beginning of the located substring,
  *         or NULL if the substring is not found.
  */
-char *_strstr(char *haystack, char *needle)
+	char *_strstr(char *haystack, char *needle)
 {
 	char *start_haystack;
 	char *start_needle;
