796f0e1b2055200beba4d0a0e0c3cca4b517dc9e
tehran88 <tehranabilovaa@gmail.com>
2024-11-14 13:08:47 +0000

    memset

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,30 +1,32 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
- * _strstr -  a function that locates a substring.
- * @haystack: an input string to search in
- * @needle: an input string to locate into string haystack
- * Return:  a pointer to the beginning of the located substring,
- * or NULL if the substring is not found.
+#include <stdio.h>/**
+ * _strstr - Locates a substring.
+ * @haystack: The string to search in.
+ * @needle: The substring to locate.
+ *
+ * Return: A pointer to the beginning of the located substring,
+ *         or NULL if the substring is not found.
  */
 char *_strstr(char *haystack, char *needle)
 {
-	char *startn = needle, *starth = haystack;
-
+	char *start_haystack;
+	char *start_needle;if (*needle == '\0')
+	{
+	return (haystack);
+	}
 	while (*haystack)
 	{
-		starth = haystack;
-		needle = startn;
-		while (*haystack == *needle)
-		{
-			haystack++;
-			needle++;
-		}
-
-		if (*needle == '\0')
-			return (haystack);
-		haystack = starth + 1;
+	start_haystack = haystack;
+	start_needle = needle;
+	while (*haystack && *needle && *haystack == *needle)
+	{
+		haystack++;
+		needle++;
+	}	if (*needle == '\0')
+	{
+		return (start_haystack);
+	}	haystack = start_haystack + 1;
+	needle = start_needle;
 	}
 	return (NULL);
 }
