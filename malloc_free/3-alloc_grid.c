1a954edf979cf6a824da3de03ccbfd08559f8de4
tehran88 <tehranabilovaa@gmail.com>
2024-11-22 12:39:54 +0000

    malloc

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -8,13 +8,13 @@
  */
 void free_grid(int **array, int height)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < height; i++)
-    {
-        free(array[i]);
-    }
-    free(array);
+	for (i = 0; i < height; i++)
+	{
+		free(array[i]);
+	}
+	free(array);
 }
 
 /**
@@ -26,42 +26,42 @@ void free_grid(int **array, int height)
  */
 int **alloc_grid(int width, int height)
 {
-    int **array;
-    int i, j;
+	int **array;
+	int i, j;
 
-    /* Check if the width or height is less than or equal to 0 */
-    if (width <= 0 || height <= 0)
-    {
-        return (NULL);
-    }
+	/* Check if the width or height is less than or equal to 0 */
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
 
-    /* Allocate memory for an array of height pointers to integers */
-    array = (int **)malloc(sizeof(int *) * height);
-    if (array == NULL)
-    {
-        return (NULL);
-    }
+	/* Allocate memory for an array of height pointers to integers */
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 
-    /* Allocate memory for each row of the 2D array */
-    for (i = 0; i < height; i++)
-    {
-        array[i] = (int *)malloc(sizeof(int) * width);
-        if (array[i] == NULL)
-        {
-            /* If memory allocation fails, free the previously allocated memory */
-            free_grid(array, i);
-            return (NULL);
-        }
-    }
+	/* Allocate memory for each row of the 2D array */
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			/* If memory allocation fails, free the previously allocated memory */
+			free_grid(array, i);
+			return (NULL);
+		}
+	}
 
-    /* Initialize all elements of the 2D array to 0 */
-    for (i = 0; i < height; i++)
-    {
-        for (j = 0; j < width; j++)
-        {
-            array[i][j] = 0;
-        }
-    }
+	/* Initialize all elements of the 2D array to 0 */
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
 
-    return (array);
+	return (array);
 }
