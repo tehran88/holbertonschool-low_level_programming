69e64a03a6eaae7deb8ba9132a2a4dcbdd92cd37
tehran88 <tehranabilovaa@gmail.com>
2024-11-22 12:23:57 +0000

    malloc

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,45 +10,46 @@
  */
 int **alloc_grid(int width, int height)
 {
-    int **array, i, j;
+int **array, i, j;
 
-    /* Check if the width or height is less than or equal to 0 */
-    if (width <= 0 || height <= 0)
-    {
-        return (NULL);
-    }
+	/* Check if the width or height is less than or equal to 0 */
+	if (width <= 0 || height <= 0) 
+{
+	return (NULL);
+}
 
-    /* Allocate memory for an array of height pointers to integers */
-    array = (int **)malloc(sizeof(int *) * height);
-    if (array == NULL)
-    {
-        return (NULL);
-    }
+	/* Allocate memory for an array of height pointers to integers */
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+{
+	return (NULL);
+}
 
-    /* Allocate memory for each row of the 2D array */
-    for (i = 0; i < height; i++)
-    {
-        array[i] = (int *)malloc(sizeof(int) * width);
-        if (array[i] == NULL)
-        {
-            /* If memory allocation fails, free the previously allocated memory */
-            for (j = 0; j < i; j++)
-            {
-                free(array[j]);
-            }
-            free(array);
-            return (NULL);
-        }
-    }
+	/* Allocate memory for each row of the 2D array */
+	for (i = 0; i < height; i++)
+{
+	array[i] = (int *)malloc(sizeof(int) * width);
+	if (array[i] == NULL)
+{
+	/* If memory allocation fails, free the previously allocated memory */
+	for (j = 0; j < i; j++)
+{
+	free(array[j]);
+}
+	free(array);
+	return (NULL);
+}
+}
 
-    /* Initialize all elements of the 2D array to 0 */
-    for (i = 0; i < height; i++)
-    {
-        for (j = 0; j < width; j++)
-        {
-            array[i][j] = 0;
-        }
-    }
+	/* Initialize all elements of the 2D array to 0 */
+	for (i = 0; i < height; i++)
+{
+	for (j = 0; j < width; j++)
+{
+	array[i][j] = 0;
+}
+}
 
-    return (array);
+	return (array);
+
 }
