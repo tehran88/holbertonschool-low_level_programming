d0cbf1266b4f3d75eb6644c40aefcaa1d1bd3cfa
tehran88 <tehranabilovaa@gmail.com>
2024-11-22 15:15:49 +0000

    C - More malloc, free

--- main.h
+++ main.h
@@ -1,17 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include<stdio.h>
-int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 #endif
