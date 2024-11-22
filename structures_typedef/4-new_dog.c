9820360970229c0d80bb0c12eeb342dd2be29a14
tehran88 <tehranabilovaa@gmail.com>
2024-11-22 16:42:03 +0000

    hh

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,49 +1,102 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * new_dog - creates a new dog structure
- * @name: Dog's name
- * @age: Dog's age
- * @owner: Dog's owner
- * Return: returns a pointer to newly created dog structure
+ * name_copy - function
+ * @name: pointer
+ * Return: name copy
+ */
+char *name_copy(char *name)
+{
+	int i;
+	char *n;
+
+	i = 0;
+	while (name[i] != 0)
+	{
+		i++;
+	}
+	n = malloc(i + 1);
+	if (n == NULL)
+	{
+		free(n);
+		return (NULL);
+	}
+	i = 0;
+	while (name[i] != 0)
+	{
+		n[i] = name[i];
+		i++;
+	}
+	n[i] = '\0';
+
+	return (n);
+}
+
+/**
+ * owner_copy - func
+ * @owner: pointer
+ * Return: owner copy
+ */
+char *owner_copy(char *owner)
+{
+	int i;
+	char *o;
+
+	i = 0;
+	while (owner[i] != 0)
+	{
+		i++;
+	}
+	o = malloc(i + 1);
+	if (o == NULL)
+	{
+		free(o);
+		return (NULL);
+	}
+	i = 0;
+	while (owner[i] != 0)
+	{
+		o[i] = owner[i];
+		i++;
+	}
+	o[i] = '\0';
+
+	return (o);
+}
+
+/**
+ * new_dog - function
+ * @age: variable
+ * @owner: pointer
+ * @name: pointer
+ * Return: new_dog info
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *cpyname, *cpyowner;
-	int len_name = 0, len_owner = 0, i;
+	dog_t *t;
+	char *n;
+	char *o;
 
-	if (name == NULL || owner == NULL)
+	t = malloc(sizeof(dog_t));
+	if (t == NULL)
+	{
+		free(t);
 		return (NULL);
-
-	while (name[len_name])
-		len_name++;
-	while (owner[len_owner])
-		len_owner++;
-
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	}
+	n = name_copy(name);
+	o = owner_copy(owner);
+	if (n == NULL || o == NULL)
+	{
+		free(o);
+		free(n);
+		free(t);
 		return (NULL);
+	}
+	t->name = n;
+	t->age = age;
+	t->owner = o;
 
-	cpyname = malloc(len_name + 1);
-	if (cpyname == NULL)
-		return (NULL);
-	for (i = 0; name[i]; i++)
-		cpyname[i] = name[i];
-	cpyname[i] = '\0';
-
-	cpyowner = malloc(len_owner + 1);
-	if (cpyowner == NULL)
-		return (NULL);
-	for (i = 0; owner[i]; i++)
-		cpyowner[i] = owner[i];
-	cpyowner[i] = '\0';
-
-	new_dog->name = cpyname;
-	new_dog->age = age;
-	new_dog->owner = cpyowner;
-	free(cpyname);
-	free(cpyowner);
-	return (new_dog);
+	return (t);
 }
