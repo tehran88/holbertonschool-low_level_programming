a28cac437d0c0b3f67df09800df5f984c7e935b3
tehran88 <tehranabilovaa@gmail.com>
2024-12-06 21:02:04 +0400

    vsvsd

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,18 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * list_len - A function that gets the number of nodes of linked list
- * @h: head of linked list
- * Return: number of nodes as size_t
+ * list_len - main
+ * @h: clone of our  linkedlist
+ * Return: length
  */
+
 size_t list_len(const list_t *h)
 {
-	unsigned int nodes = 0;
+	int lengthoflinkedlist = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		nodes++;
+		lengthoflinkedlist++;
 		h = h->next;
 	}
-	return (nodes);
+	return (lengthoflinkedlist);
 }
