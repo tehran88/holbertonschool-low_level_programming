379b4be33b00d2e39f70a8d8bd479215e0610d49
tehran88 <tehranabilovaa@gmail.com>
2024-12-07 15:41:01 +0000

    Change

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,57 +1,41 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - entry point
- * @argc: argument count
- * @argv: array of arguments
- *
- * Return: 0 on success
+ * main - cp
+ * @argc: counter of args
+ * @argv: args
+ * Return: 0
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, rd_sz = 1, wr_sz;
-	mode_t permissions = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
-	char buffer[BUFSIZ];
+	int o, o1, r;
+	char *buff[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	o = open(argv[1], O_RDONLY);
+	o1 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((r = read(o, buff, 1024)) > 0)
+	{
+		if (o1 == -1 || write(o1, buff, r) != r)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (r == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, permissions);
-	if (fd_to == -1)
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	while (rd_sz)
+	if (close(o) == -1 || close(o1) == -1)
 	{
-		rd_sz = read(fd_from, buffer, BUFSIZ);
-		if (rd_sz == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		if (rd_sz > 0)
-		{
-			wr_sz = write(fd_to, buffer, rd_sz);
-			if (wr_sz != rd_sz || wr_sz == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-		}
+		dprintf(STDERR_FILENO, "Error: Can't close fd -1\n");
+		exit(100);
 	}
-	if (close(fd_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
-	if (close(fd_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
-
-
 	return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -1,13 +1,14 @@
-#ifndef H_H
-#define H_H
+#ifndef MAIN_H
+#define MAIN_H
 
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
 
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
